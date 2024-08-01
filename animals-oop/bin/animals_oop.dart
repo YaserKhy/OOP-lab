c1e633d0e71e36263506509b53f194274a3050a0
Yaser Alkhayyat <yaserkhayyat2017@gmail.com>
2024-08-01 13:58:40 +0300

    menu creation

--- animals-oop/bin/animals_oop.dart
+++ animals-oop/bin/animals_oop.dart
@@ -1,3 +1,14 @@
+// imports
+import 'dart:io';       // library to interact with user
+
 void main() {
-  print("Hello oop !");
+  print("----> Welcome to our animals shop ! <----");
+  String? choice = stdin.readLineSync();
+  while_loop:
+  while(true) {
+    switch(choice) {
+      case '0' :
+      break while_loop;
+    }
+  }
 }
