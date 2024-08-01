9301b5dddab8f8a5cee60c50563f4cd96560fdd4
Ammarx9 <ammarxx919@gmail.com>
2024-08-01 13:40:33 +0300

    animal class redy

---
+++ animals-oop/lib/animal_model.dart
@@ -0,0 +1,42 @@
+
+
+
+
+
+
+class animalModel{
+
+
+  String name;
+  int limp;
+  double price;
+  String food;
+
+
+  animalModel({required this.name , required this.limp , required this.price , required this.food });
+
+
+
+  getName(){
+    print("\n\n######## animal name ------> $name  ########");
+  }
+
+  getLimp(){
+    print("\n\n######## animal limp ------> $limp  ########");
+  }
+
+
+  getPricee(){
+    print("\n\n######## animal price ------> $price  ########" );
+  }
+
+
+getFood(){
+    print("\n\n######## animal food ------> $food ########");
+  }
+
+
+
+
+
+}
